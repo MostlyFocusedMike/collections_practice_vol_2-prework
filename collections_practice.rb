50cd4b0a484ec207f8f19690c767868bb05e9c4c
Mike C <mikecronin92@gmail.com>
2018-03-28 00:27:44 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
+require 'pry'
+
 # your code goes here
+def begins_with_r(arr)
+  return !(arr.any? {|word| word[0] != "r"})
+end  
+
+def contain_a(arr)
+  arr.map {|word| word if word.include?("a")}.compact
+end
+
+def first_wa(arr)
+  arr.find {|word| word[0..1] =="wa"}
+end
+
+def remove_non_strings(arr)
+  arr.delete_if {|val| val.class != String}
+end
+
+def count_elements(*hashes)
+  hashes.map do |hash|
+    hashes.count(hash)
+  end
+end
+hashes = [{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
+binding.pry
