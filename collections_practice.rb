c70fcb159768abf0d922b5a5007f5bcae7d621e4
Mike C <mikecronin92@gmail.com>
2018-03-28 01:57:13 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,8 +27,9 @@ end
 def merge_data(keys,data)
   counter = 0
   data[0].map do |name,hash|
-    hash.merge(keys[counter])
     counter += 1
+    hash.merge(keys[counter -1])
+    
   end
 end
 [{:first_name => "blake"},
