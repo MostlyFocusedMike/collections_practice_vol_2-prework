1918bfd1655ed3f97761831d126f80ed67005f0c
Mike C <mikecronin92@gmail.com>
2018-03-28 00:49:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,8 +18,7 @@ def remove_non_strings(arr)
 end
 
 def count_elements(hashes)
-  fin = hashes.uniq
-  fin.map do |item|
+  hashes.uniq.map do |item|
     item[:count] = hashes.count(item)
     item
   end
