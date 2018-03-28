cab15e2fc6f0fbbebf3dc693626f3a39994e6e6a
Mike C <mikecronin92@gmail.com>
2018-03-28 01:33:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -29,5 +29,18 @@ def merge_data(keys,data)
     hash.merge(keys[i])  
   end
 end
+
+{
+             "blake" => {
+                :awesomeness => 10,
+                     :height => "74",
+                  :last_name => "johnson"
+            },
+            "ashley" => {
+                :awesomeness => 9,
+                     :height => 60,
+                  :last_name => "dubs"
+            }
+        }
 [{:first_name=>"blake", :awesomeness=>10, :height=>"74", :last_name=>"johnson"}, {:first_name=>"ashley",:awesomeness=>9, :height=>60, :last_name=>"dubs"}]
 hashes = [{:name => "blake"}, {:name => "blake"}, {:name => "ashley"}]
